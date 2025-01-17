a569cb1eb19ceb3a27a3f185844726d655283e99
Abeagom <beas.gomez.alberto@iescamas.es>
2025-01-18 00:02:13 +0100

    Añadidos primeros tests de Termómetro

--- termometro_proyecto/src/test/java/com/endes/instrumento/TermometroTest.java
+++ termometro_proyecto/src/test/java/com/endes/instrumento/TermometroTest.java
@@ -2,13 +2,32 @@ package com.endes.instrumento;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class TermometroTest {
+	Termometro t = new Termometro(36);
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	@DisplayName("Prueba del método get temperatura") 
+	void testGet() {
+		double resultadoEsperado = 36;
+		assertEquals(resultadoEsperado, t.getTemperaturaCelsius(), "La temperatura no coincide");
+	}
+	
+	@Test
+	@DisplayName("Prueba del método set temperatura") 
+	void testSet() {
+		double resultadoEsperado = 37;
+		t.setTemperaturaCelsius(37);
+		assertEquals(resultadoEsperado, t.getTemperaturaCelsius(), "La temperatura no coincide");
+	}
+	
+	@Test
+	@DisplayName("Prueba válida de convertir a Farenheit") 
+	void testValidoConvertirAFarenheit() {
+		double resultadoEsperado = 96.8;
+		assertEquals(resultadoEsperado, t.convertirAFahrenheit(), "La temperatura no coincide");
 	}
 
 }
